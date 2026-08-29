e1070f9b53a5b3601357362f08cd74e12de9d6cd
LK9T9-Sektor <LK9T9_Sektor@mail.ru>
2026-08-29 13:36:41 +0300

    Add Core.Data library with JsonAI brain reader.

---
+++ src/Core/Sektor.DarkestDungeon.Core.Data/Dto/JsonSelectionDesire.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Sektor.DarkestDungeon.Core.Data.Dto
+{
+    /// <summary>A single desire entry (skill/target/bonus) of a JsonAI.json monster brain.</summary>
+    public class JsonSelectionDesire
+    {
+        /// <summary>Gets or sets the desire type key that selects its implementation.</summary>
+        public string type { get; set; }
+
+        /// <summary>Gets or sets the desire parameter data set.</summary>
+        public Dictionary<string, object> data { get; set; }
+    }
+}
